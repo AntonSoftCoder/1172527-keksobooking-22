1529a39d3401cdce57d5d4da63e42308e200ee82
Student <studentfrontend20@gmail.com>
2021-02-09 15:52:21 +0100

    Random functions code review fixes

--- js/main.js
+++ js/main.js
@@ -1,28 +1,35 @@
-const MAX_FLOAT_PRECISION = 17;
+const MAX_FLOAT_PRECISION = Math.pow(10, 17);
 
-getRandomCoordinate(48.13037236984187, 49.132707733425015, 5);
+function getRandomNumber(from, to) {
+  const fromInt = Number(from);
+  const toInt = Number(to);
+  if (fromInt < 0 || toInt < 0 || fromInt > toInt || !Number.isInteger(fromInt) || !Number.isInteger(toInt)) {
+    throw new Error(`from = ${from}, to = ${to} must make a positive integer range`);
+  }
+
+  if (fromInt === toInt) {
+    return fromInt;
+  }
+
+  return fromInt + Math.floor(Math.random()*(toInt - fromInt + 1));
+}
 
 function getRandomCoordinate(from, to, floatPoints) {
-  if (from < 0 || to < 0 || from >= to || typeof from !== 'number' || typeof to !== 'number') {
+  if (from < 0 || to < 0 || from > to || isNaN(from) || isNaN(to)) {
     throw new Error(`from = ${from}, to = ${to} must make a positive range`);
   }
 
   if (floatPoints < 0 || !Number.isInteger(floatPoints)) {
     throw new Error(`floatPoints = ${floatPoints} must be a non-negative integer`);
   }
-  const fromInt = parseInt(from * Math.pow(10, MAX_FLOAT_PRECISION));
-  const toInt = parseInt(to * Math.pow(10, MAX_FLOAT_PRECISION));
-  const result = getRandomNumber(fromInt, toInt) / Math.pow(10, MAX_FLOAT_PRECISION);
+
+  const fromInt = from * MAX_FLOAT_PRECISION;
+  const toInt = to * MAX_FLOAT_PRECISION;
+  const result = getRandomNumber(fromInt, toInt) / MAX_FLOAT_PRECISION;
 
   return Number(result.toFixed(floatPoints));
 }
 
-function getRandomNumber(from, to) {
-  if (from < 0 || to < 0 || from >= to || !Number.isInteger(from) || !Number.isInteger(to)) {
-    throw new Error(`from = ${from}, to = ${to} must make a positive integer range`);
-  }
-
-  return from + Number.parseInt(Math.random()*(to - from + 1));
-}
-
+getRandomCoordinate(48.13037236984187, 49.132707733425015, 5);
+getRandomCoordinate('50.13037236984187', '50.13037236984187', 4);
 
